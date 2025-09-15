efbb3e85ecb56aefb904381baa532946596dc74f
Никита <Nikita@MacBook-Pro-Best.local>
2025-09-15 18:48:13 +0300

    a

---
+++ mem.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd /home/user/lab5
+
+: > report.log
+declare -a A=()
+step=0
+
+SEQ=(1 2 3 4 5 6 7 8 9 10)
+
+while :; do
+  A+=( "${SEQ[@]}" )
+  ((step++))
+  if (( step % 100000 == 0 )); then
+    echo "${#A[@]}" >> report.log
+  fi
+done
